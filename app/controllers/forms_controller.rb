1593bbed6b313349380c796961132b76f956d376
Adam Żaczek <adamzaczekul@gmail.com>
2016-01-13 23:00:36 +0000

    Adding model changes

--- app/controllers/forms_controller.rb
+++ app/controllers/forms_controller.rb
@@ -1,14 +1,38 @@
 class FormsController < ApplicationController
     
-    def new
-        @form = Form.new
-    end
+  def new
+    @form = Form.new
+  end
 
 
-    def create
-        @form = Form.new
+  def create
+    @form = Form.new(allowed_params)
+    if @form.save
+      flash[:info] = "Succes"
+      redirect_to root_url
+    else
+      render 'new'
     end
+  end
+  
+  def edit #display for for existing record
+    @form = Form.find(params[:id])
+  end
+  
+  def update #save changes    #no template
+    @form = Form.find(params[:id])
+    if @form.update_attributes(allowed_params)
+      flash[:info] = "Formularz został poprawiony i wysłany ponownie."
+      redirect_to root_url
+    else
+      render 'new'
+    end
+  end
     
-    
-    
+     private
+  
+  def allowed_params
+    params.require(:form).permit(:opis, :kategoria, :email)
+  end
+
 end
