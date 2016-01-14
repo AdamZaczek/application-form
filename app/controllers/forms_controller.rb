e3fbeda57eb8fbf3f72cfbb4b5fdd404cb3663cd
Adam Żaczek <adamzaczekul@gmail.com>
2016-01-14 04:22:29 +0000

    Creating some base styling

--- app/controllers/forms_controller.rb
+++ app/controllers/forms_controller.rb
@@ -8,25 +8,13 @@ class FormsController < ApplicationController
   def create
     @form = Form.new(allowed_params)
     if @form.save
-      redirect_to root_url, notice: 'Form is valid'
-    else
-      render 'new', notice: 'Form is invalid'
-    end
-  end
-  
-  def edit #display for for existing record
-    @form = Form.find(params[:id])
-  end
-  
-  def update #save changes    #no template
-    @form = Form.find(params[:id])
-    if @form.update_attributes(allowed_params)
-      flash[:info] = "Formularz został poprawiony i wysłany ponownie."
-      redirect_to root_url
+      flash[:success] = 'Formularz wysłany'
+      render 'new'
     else
       render 'new'
     end
   end
+
     
      private
   
