e994681e44e3db287ae6e4351547f127351ad1aa
Adam Żaczek <adamzaczekul@gmail.com>
2016-01-16 18:59:18 +0000

    Updating read.rb

--- app/services/read.rb
+++ app/services/read.rb
@@ -1,6 +1,5 @@
 class Read
   def oldest_unread_forms(count, sort = {})
-      forms = Forms.all
       forms = forms.order(id: :asc).where(przeczytane: false).limit(count).update_all(przeczytane: true)
       forms = forms.where(kategoria: sort[:kategoria]) if sort[:kategoria].present?
       forms = forms.where(koszt: sort[:koszt]) if sort[:koszt].present?
